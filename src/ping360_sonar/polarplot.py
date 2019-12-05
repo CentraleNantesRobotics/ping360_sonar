4a037d417a7d8260c802094a63f4068efb23ceb1
Stormiix <madadj4@gmail.com>
2019-12-05 20:48:04 +0100

    cleanup and correct params values

--- src/ping360_sonar/polarplot.py
+++ src/ping360_sonar/polarplot.py
@@ -13,7 +13,6 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import rospy
 
-          
 def main():
      parser = argparse.ArgumentParser(description="Ping python library example.")
      parser.add_argument('--device', action="store", required=True, type=str, help="Ping device port.")
@@ -24,35 +23,50 @@ def main():
      args = parser.parse_args()
      try:
           rospy.init_node('ping360_node')
-          transmitFrequency = 1000
-          samplePeriod = 80
-          numberOfSample = 200
-          speedOfSound = 1450
-          length = args.size 
+          
+          # Ping 360 Parameters
+          device = "/dev/ttyUSB0"
+          baudrate = 115200
+          gain = 0
+          numberOfSamples = 1200 # Numbre of points
+          transmitFrequency = 740 # Default frequency
+          sonarRange = 30 # in m
+          speedOfSound = 1500 # in km/h ?
+          samplePeriod = calculateSamplePeriod(sonarRange, numberOfSamples, speedOfSound)
+          transmitDuration = adjustTransmitDuration(sonarRange, samplePeriod, speedOfSound)
+
+          # Output and ROS parameters
           step = 1
-          angle = 0
           topic = "ping360_sonar"
+          imgSize = args.size
           queue_size= 1
 
+          # Global Variables
+          angle = 0
           p = Ping360(args.device, args.baudrate)
           imagePub = rospy.Publisher(topic, Image, queue_size=queue_size)
           bridge = CvBridge()
 
+          # Initialize and configure the sonar
           print("Initialized: %s" % p.initialize())
+          p.set_gain_setting(gain)
           p.set_transmit_frequency(transmitFrequency)
+          p.set_transmit_duration(transmitDuration)
           p.set_sample_period(samplePeriod)
-          p.set_number_of_samples(numberOfSample)
+          p.set_number_of_samples(numberOfSamples)
 
-          max_range = samplePeriod * numberOfSample * speedOfSound / 2
-          image = np.zeros((length, length, 1), np.uint8)
-          center = (float(length/2),float(length/2))
+          # Create a new mono-channel image
+          image = np.zeros((imgSize, imgSize, 1), np.uint8)
+
+          # Center point coordinates
+          center = (float(imgSize/2),float(imgSize/2))
           while not rospy.is_shutdown():
                p.transmitAngle(angle)
                data = bytearray(getattr(p,'_data'))
                data_lst = [k for k in data]
-               linear_factor = float(len(data_lst))/float(center[0])
-               for i in range(int(center[0])):
-                    if(i < center[0]*max_range/max_range):
+               linear_factor = float(len(data_lst)) / float(center[0]) #TODO: this should probably be range/pixelsize
+               for i in range(int(center[0])): #TODO: check the update polar logic on ping-viewer
+                    if(i < center[0]):
                          pointColor = data_lst[int(i*linear_factor-1)]
                     else:
                          pointColor = 0
@@ -78,3 +92,42 @@ def publish(image, imagePub, bridge):
           imagePub.publish(bridge.cv2_to_imgmsg(image, "mono8"))
      except CvBridgeError as e:
           print(e)
+
+# https://discuss.bluerobotics.com/t/please-provide-some-answer-regards-ping360/6393/3?u=stormix
+
+def calculateSamplePeriod(distance: int, numberOfSamples: int, speedOfSound: int, _samplePeriodTickDuration = 25e-9) -> float:
+     """
+      Calculate the sample period based in the new range
+     """
+     return 2*distance/(numberOfSamples*speedOfSound*_samplePeriodTickDuration)
+
+def adjustTransmitDuration(distance: int, samplePeriod: float, speedOfSound: int, _firmwareMinTransmitDuration = 5) -> float:
+     """
+     @brief Adjust the transmit duration for a specific range
+     Per firmware engineer:
+     1. Starting point is TxPulse in usec = ((one-way range in metres) * 8000) / (Velocity of sound in metres
+     per second)
+     2. Then check that TxPulse is wide enough for currently selected sample interval in usec, i.e.,
+          if TxPulse < (2.5 * sample interval) then TxPulse = (2.5 * sample interval)
+     3. Perform limit checking
+     @return Transmit duration
+     """
+     # 1
+     duration = 8000 * distance / speedOfSound
+     # 2 (transmit duration is microseconds, samplePeriod() is nanoseconds)
+     transmit_duration = max(2.5*samplePeriod()/1000, duration)
+     # 3
+     return max(_firmwareMinTransmitDuration, min(transmitDurationMax(samplePeriod), transmit_duration))
+
+def transmitDurationMax(samplePeriod: float, _firmwareMaxTransmitDuration = 500) -> float:
+     """
+     @brief The maximum transmit duration that will be applied is limited internally by the
+     firmware to prevent damage to the hardware
+     The maximum transmit duration is equal to 64 * the sample period in microseconds
+     @return The maximum transmit duration possible
+     """
+     return min(_firmwareMaxTransmitDuration, samplePeriod(samplePeriod) * 64e6)
+
+def samplePeriod(samplePeriod: float, _samplePeriodTickDuration = 25e-9) -> float:
+     """  Sample period in ns """
+     return samplePeriod*_samplePeriodTickDuration
