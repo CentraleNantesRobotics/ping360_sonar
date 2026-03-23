739c43adf49701c60dfd8c8dcc1d041ed9921815
Paul Joseph <paul@bubble-robotics.com>
2026-03-23 15:37:18 +0000

    fix launchfile

--- ping360_sonar/launch/distance_measure.launch.py
+++ ping360_sonar/launch/distance_measure.launch.py
@@ -10,8 +10,8 @@ def generate_launch_description():
         package = "ping360_sonar",
         executable = "ping360_node",
         parameters = [
-            {"range_max": 5},
-            {"angle_sector": 60}
+            {"range_max": 1},
+            {"angle_sector": 120}
         ]
     )
 
