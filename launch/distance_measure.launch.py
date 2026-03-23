c3d00d2450d1d2c33d83d7bff060adef37d4f056
Cedric Landsmeer <clandsmeer@student.ethz.ch>
2026-03-23 15:18:10 +0000

    add launch file skeleton

---
+++ launch/distance_measure.launch.py
@@ -0,0 +1,20 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    ld = LaunchDescription()
+
+    pingNode = Node(
+        package = "ping360_sonar",
+        executable = "ping360_node",
+        parameters = [
+            {"range_max": 5},
+            {"angle_sector": 60}
+        ]
+    )
+
+    ld.add_action(pingNode)
+
+    return ld
